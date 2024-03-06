8722d5be3b3d7c4b852c364935f751056422c3a3
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-03-06 15:23:48 +0800

    schema writing practice

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 
@@ -7,6 +8,75 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// schema design
+mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name for this product."],
+        unique: [true, "Name must be unique."],
+        trim: true,
+        minLength: [3, "Name must be at least 3 characters."],
+        maxLength: [100, "Name is too large."]
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, "Price can't be negative."]
+    },
+    unit: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["kg", "litre", "pcs"],
+            message: "unit value can't be {VALUE}, must be kg/litre/pcs"
+        }
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: [0, "Quantity can't be negative."],
+        validate: {
+            validator: (value) => {
+                const isInteger = Number.isInteger(value);
+                if(isInteger){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        },
+        message: "Quantity must be an integer."
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ["in-stock", "out-of-stock", "discontinued"],
+            message: "status can't be {VALUE}"
+        }
+    },
+    // reference to supplier model/schema | Example
+    supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Supplier"
+    },
+    // array of object schema
+    categories: [{
+        name: {
+            type: String,
+            required: true
+        },
+        _id: mongoose.Schema.Types.ObjectId
+    }]
+}, {
+    timestamps: true
+});
+
+// SCHEMA -> MODEL -> QUERY
 
 // initial route
 app.get("/", (req, res) => {
