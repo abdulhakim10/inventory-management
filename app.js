52ffd4f6ba038a2f83aea2cb9442d4b572601917
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-03-08 19:59:54 +0800

    middleware and instance practice

--- app.js
+++ app.js
@@ -60,38 +60,104 @@ const productSchema = mongoose.Schema({
         }
     },
     // reference to supplier model/schema | Example
-    supplier: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Supplier"
-    },
-    // array of object schema
-    categories: [{
-        name: {
-            type: String,
-            required: true
-        },
-        _id: mongoose.Schema.Types.ObjectId
-    }]
+    // supplier: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "Supplier"
+    // },
+    // // array of object schema
+    // categories: [{
+    //     name: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     _id: mongoose.Schema.Types.ObjectId
+    // }]
 }, {
     timestamps: true
 });
 
+// mongoose middleware for saving data: pre / post.
+productSchema.pre("save", function(next){
+    console.log("Before saving data");
+    if(this.quantity == 0){
+        this.status = "out-of-stock";
+    }
+
+    next()
+});
+
+// productSchema.post("post", function(doc, next) {
+//     console.log("After saving data.")
+// })
+
+// instance
+productSchema.methods.logger = function(){
+    console.log(`Data saved for ${this.name}`);
+}
+
 // SCHEMA -> MODEL -> QUERY
+
+// creating model
+const Product = mongoose.model("Product", productSchema);
+
+
 
 // initial route
 app.get("/", (req, res) => {
     res.send("Route is working Yay!!")
 });
 
-app.post("/api/products", (req, res) => {
-    res.send("Product added successfully");
+// create or save method
+app.post("/api/v1/product",  async(req, res, next) => {
+    try{
+    // creating instance of product model
+    // const product = new Product(req.body);
+    // if (product.quantity == 0) {
+    //     product.status = "out-of-stock";
+    // }
+    // product save on database
+    // const result = await product.save();
+
+    // product save on database using create method
+    const result = await Product.create(req.body)
+    result.logger();
+
+    res.status(201).json({
+        success: true,
+        message: "Product created successfully.",
+        data: result
+    });      
+    }
+    catch(error){
+        next(error);
+    }
 });
 
-app.get("/api/products", (req, res) => {
-    res.send("All products");
+app.delete("/api/v1/product", async(req, res, next) => {
+    const id = req.params.id;
+    const query = {};
+    res.status(200).json({
+        success: true,
+        message: "Product deleted successfully."
+    });
 });
 
-app.get("/api/products/:id", (req, res) => {
+app.get("/api/v1/:id", (req, res) => {
+    try{
+        const id = req.params.id;
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid product id."
+        });
+    }
     res.send("Single product");
+    }
+    catch(error){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
 });
 module.exports = app;
